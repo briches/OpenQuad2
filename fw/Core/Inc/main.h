c6291b9d00d858d0dc12ad9fa40564f8dd5c39e7
briches <richesbc@gmail.com>
2020-12-02 20:09:05 -0700

    Add existing hardware and firmware files

---
+++ fw/Core/Inc/main.h
@@ -0,0 +1,151 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define FLASH_NRESET_Pin GPIO_PIN_3
+#define FLASH_NRESET_GPIO_Port GPIOE
+#define IMU_AG_DEN_Pin GPIO_PIN_4
+#define IMU_AG_DEN_GPIO_Port GPIOE
+#define IMU_SDO_AG_Pin GPIO_PIN_5
+#define IMU_SDO_AG_GPIO_Port GPIOE
+#define IMU_SDO_MAG_Pin GPIO_PIN_6
+#define IMU_SDO_MAG_GPIO_Port GPIOE
+#define EVENT_IN_Pin GPIO_PIN_0
+#define EVENT_IN_GPIO_Port GPIOC
+#define VBAT_SENSE_Pin GPIO_PIN_3
+#define VBAT_SENSE_GPIO_Port GPIOC
+#define WIFI_SER_CFG_Pin GPIO_PIN_5
+#define WIFI_SER_CFG_GPIO_Port GPIOC
+#define LED_B_Pin GPIO_PIN_0
+#define LED_B_GPIO_Port GPIOB
+#define LED_G_Pin GPIO_PIN_1
+#define LED_G_GPIO_Port GPIOB
+#define LED_R_Pin GPIO_PIN_2
+#define LED_R_GPIO_Port GPIOB
+#define BLDC_BOOTSEL1_Pin GPIO_PIN_10
+#define BLDC_BOOTSEL1_GPIO_Port GPIOD
+#define BLDC_BOOTSEL2_Pin GPIO_PIN_11
+#define BLDC_BOOTSEL2_GPIO_Port GPIOD
+#define BLDC_BOOTSEL3_Pin GPIO_PIN_12
+#define BLDC_BOOTSEL3_GPIO_Port GPIOD
+#define BLDC_BOOTSEL4_Pin GPIO_PIN_13
+#define BLDC_BOOTSEL4_GPIO_Port GPIOD
+#define WIFI_CHIP_EN_Pin GPIO_PIN_6
+#define WIFI_CHIP_EN_GPIO_Port GPIOC
+#define WIFI_RESETN_Pin GPIO_PIN_7
+#define WIFI_RESETN_GPIO_Port GPIOC
+#define BLDC_ARM4_Pin GPIO_PIN_8
+#define BLDC_ARM4_GPIO_Port GPIOA
+#define BLDC_ARM3_Pin GPIO_PIN_10
+#define BLDC_ARM3_GPIO_Port GPIOA
+#define BLDC_ARM2_Pin GPIO_PIN_15
+#define BLDC_ARM2_GPIO_Port GPIOA
+#define BLDC_ARM1_Pin GPIO_PIN_0
+#define BLDC_ARM1_GPIO_Port GPIOD
+#define BLDC_nRESET1_Pin GPIO_PIN_1
+#define BLDC_nRESET1_GPIO_Port GPIOD
+#define BLDC_nRESET2_Pin GPIO_PIN_3
+#define BLDC_nRESET2_GPIO_Port GPIOD
+#define BLDC_nRESET3_Pin GPIO_PIN_4
+#define BLDC_nRESET3_GPIO_Port GPIOD
+#define BLDC_nRESET4_Pin GPIO_PIN_5
+#define BLDC_nRESET4_GPIO_Port GPIOD
+#define BARO_INT_Pin GPIO_PIN_6
+#define BARO_INT_GPIO_Port GPIOD
+#define BARO_INT_EXTI_IRQn EXTI9_5_IRQn
+#define IMU_MAG_DRDY_Pin GPIO_PIN_7
+#define IMU_MAG_DRDY_GPIO_Port GPIOD
+#define IMU_MAG_DRDY_EXTI_IRQn EXTI9_5_IRQn
+#define IMU_AG_INT2_Pin GPIO_PIN_9
+#define IMU_AG_INT2_GPIO_Port GPIOG
+#define IMU_AG_INT2_EXTI_IRQn EXTI9_5_IRQn
+#define IMU_AG_INT1_Pin GPIO_PIN_10
+#define IMU_AG_INT1_GPIO_Port GPIOG
+#define IMU_AG_INT1_EXTI_IRQn EXTI15_10_IRQn
+#define IMU_MAG_INT_Pin GPIO_PIN_11
+#define IMU_MAG_INT_GPIO_Port GPIOG
+#define IMU_MAG_INT_EXTI_IRQn EXTI15_10_IRQn
+#define GPS_TIME_Pin GPIO_PIN_12
+#define GPS_TIME_GPIO_Port GPIOG
+#define GPS_TIME_EXTI_IRQn EXTI15_10_IRQn
+#define GPS_EXTINT_Pin GPIO_PIN_13
+#define GPS_EXTINT_GPIO_Port GPIOG
+#define GPS_EXTINT_EXTI_IRQn EXTI15_10_IRQn
+#define WIFI_IRQ_Pin GPIO_PIN_14
+#define WIFI_IRQ_GPIO_Port GPIOG
+#define WIFI_IRQ_EXTI_IRQn EXTI15_10_IRQn
+#define GPS_RESETN_Pin GPIO_PIN_5
+#define GPS_RESETN_GPIO_Port GPIOB
+#define IMU_CS_AG_Pin GPIO_PIN_8
+#define IMU_CS_AG_GPIO_Port GPIOB
+#define IMU_CS_MAG_Pin GPIO_PIN_9
+#define IMU_CS_MAG_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ hw/Component Models/3D-Step-Models-Library-master/3D-Step-Models-Library-master/SHO/README.md
@@ -0,0 +1,57 @@
+﻿# 3D-Step-Models-Library
+Mechanical 3D Models in STEP AP214 File Format
+--------------------------------------------------------------------------------
+
+Copyright &copy; u-blox 
+
+u-blox reserves all rights in this deliverable (documentation, software, etc., 
+hereafter “Deliverable”). 
+
+u-blox grants you the right to use, copy, modify and distribute the Deliverable
+provided hereunder for any purpose without fee.  
+
+THIS DELIVERABLE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED 
+WARRANTY. IN PARTICULAR, NEITHER THE AUTHOR NOR U-BLOX MAKES ANY REPRESENTATION 
+OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY OF THIS DELIVERABLE 
+OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+
+In case you provide us a feedback or make a contribution in the form of a 
+further development of the Deliverable (“Contribution”), u-blox will have the 
+same rights as granted to you, namely to use, copy, modify and distribute the 
+Contribution provided to us for any purpose without fee.
+
+-------------------------------------------------------------------------------
+
+|FILENAME            |Applicable modules |
+|--------------------|-------------------|
+|ANNA-B112.STEP      |ANNA-B112|
+|BMD-300-330-360.STEP|BMD-300, BMD-330, BMD-360|
+|BMD-301.STEP        |BMD-301|
+|BMD-340.STEP        |BMD-340|
+|BMD-341.STEP        |BMD-341|
+|BMD-345.STEP        |BMD-345|
+|BMD-350.STEP        |BMD-350|
+|BMD-380.STEP        |BMD-380|
+|ELLA-W1.STEP        |ELLA-W131, ELLA-W133, ELLA-W161, EMMY-W163|
+|EMMY-W1.STEP        |EMMY-W161, EMMY-W163, EMMY-W165|
+|JODY-W1-W2.STEP     |JODY-W163, JODY-W164, JODY-W167, JODY-W263|
+|LILY-W131.STEP      |LILY-W131|
+|LILY-W132.STEP      |LILY-W132|
+|NINA-B111.STEP      |NINA-B111|
+|NINA-B112.STEP      |NINA-B112|
+|NINA-B221-W1x1.STEP |NINA-B221, NINA-W101, NINA-W131, NINA-W151|
+|NINA-B222-W1x2.STEP |NINA-B222, NINA-W102, NINA-W132, NINA-W152|
+|NINA-B3x1.STEP      |NINA-B301, NINA-B311|
+|NINA-B3x2.STEP      |NINA-B302, NINA-B312|
+|NINA-B3x6-B4x6.STEP |NINA-B306, NINA-B316, NINA-B406|
+|NINA-B400.STEP      |NINA-B400|
+|NINA-W1x6.STEP      |NINA-W106, NINA-W156|
+|NORA-B100.STEP      |NORA-B100|
+|NORA-B101.STEP      |NORA-B101|
+|NORA-B106.STEP      |NORA-B106|
+|ODIN-W160.STEP      |ODIN-W160|
+|ODIN-W161.STEP      |ODIN-W161|
+|ODIN-W260.STEP      |ODIN-W260|
+|ODIN-W262.STEP      |ODIN-W261|
+|R41Z.STEP           |R41Z|
+|THEO-P173.STEP      |THEO-B173|
