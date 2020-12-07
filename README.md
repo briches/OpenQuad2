0739fda9f221f38af12dd451dbca141faf3ffd14
briches <richesbc@gmail.com>
2020-12-07 13:09:00 -0700

    added block diagram and coremark fw project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OpenQuad2
+![Block Diagram](https://github.com/briches/openquad2/blob/master/blockdiagram.PNG?raw=true)
---
+++ fw/coremark/Drivers/DebugLog/module_ids.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////
+// Copyright © 2020,
+// 4iiii Innovations Inc.,
+// Cochrane, Alberta, Canada.
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are not permitted without express written approval of
+// 4iiii Innovations Inc.
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MODULE_IDS_H_
+#define MODULE_IDS_H_
+
+typedef enum
+{
+   MAIN_MODULE_ID,
+   NUM_MODULES
+} module_id_t;
+
+
+#endif
